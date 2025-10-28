4e08490f0b8afbfa16877e9c10c59f320dd49065
bhushankpmg <bhushantopre@kpmg.com>
2025-10-28 15:48:14 +0530

    Add variables.tf (generated by Azure OpenAI)

--- variables.tf
+++ variables.tf
@@ -1,66 +1,71 @@
-To create a Terraform variables configuration for an Azure Storage Account, you'll want to define several relevant variables such as the resource group name, storage account name, location, account type, and other settings. Below is an example of how you can structure your `variables.tf` file for an Azure Storage Account.
+Certainly! Below is an example of a Terraform variables configuration file (`variables.tf`) for provisioning an Azure Storage Account. This configuration includes parameters such as the storage account name, resource group name, location, SKU, and kind.
 
-### Example: `variables.tf`
+### `variables.tf`
 
 ```hcl
 variable "resource_group_name" {
-  description = "The name of the resource group in which to create the storage account."
-  type        = string
-}
-
-variable "storage_account_name" {
-  description = "The name of the storage account. The name must be between 3 and 24 characters in length and can only include lowercase letters and numbers."
+  description = "The name of the resource group where the storage account will be created."
   type        = string
 }
 
 variable "location" {
-  description = "The Azure region where the storage account will be created."
+  description = "The Azure region where the resource group and storage account will be created."
   type        = string
-  default     = "East US" # You can change this to any supported Azure region
+  default     = "East US"  # You can change the default to your preferred location
+}
+
+variable "storage_account_name" {
+  description = "The name of the storage account. Must be between 3 and 24 characters in length and can only contain lowercase letters and numbers."
+  type        = string
 }
 
 variable "account_tier" {
-  description = "The tier for the storage account. Can be 'Standard' or 'Premium'."
+  description = "The tier of the storage account."
   type        = string
-  default     = "Standard"
+  default     = "Standard"  # Options can be "Standard" or "Premium"
 }
 
-variable "account_replication_type" {
-  description = "The replication type of the storage account. Valid options: LRS, GRS, RA-GRS, GEO-ZRS, etc."
+variable "account_kind" {
+  description = "The kind of storage account."
   type        = string
-  default     = "LRS"
+  default     = "StorageV2"  # Other options can include "Storage", "BlobStorage", etc.
 }
 
-variable "enable_tls" {
-  description = "Should TLS be enabled for the storage account? This is typically true."
+variable "enable_https_traffic_only" {
+  description = "Whether to allow only secure HTTPS traffic to the storage account."
   type        = bool
   default     = true
 }
+```
 
-variable "allow_blob_public_access" {
-  description = "Indicates whether public access is allowed for blob services. Set to false for security."
-  type        = bool
-  default     = false
+### Example Usage (`main.tf`)
+
+You would typically use these variables in your main Terraform configuration file, where you define the actual resources:
+
+```hcl
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = var.storage_account_name
+  resource_group_name     = azurerm_resource_group.example.name
+  location                = azurerm_resource_group.example.location
+  account_tier            = var.account_tier
+  account_kind            = var.account_kind
+  enable_https_traffic_only = var.enable_https_traffic_only
+
+  tags = {
+    Environment = "Terraform"
+  }
 }
 ```
 
-### Example: `terraform.tfvars`
+### Example Input Values (`terraform.tfvars`)
 
-You can define your variable values in a `terraform.tfvars` file like this:
-
-```hcl
-resource_group_name       = "my-resource-group"
-storage_account_name      = "mystorageaccount"
-location                  = "East US"
-account_tier              = "Standard"
-account_replication_type  = "LRS"
-enable_tls                = true
-allow_blob_public_access   = false
-```
-
-### Usage in `main.tf`
-
-With the variables defined, you can use them when creating the Azure Storage Account resource as follows:
-
-```hcl
-provider "azurerm"
+You can specify the values for these variables in a separate
