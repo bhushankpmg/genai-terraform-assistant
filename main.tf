093a98e40a7696d85409aa5d63d5aded9b63ff24
bhushankpmg <bhushantopre@kpmg.com>
2025-11-03 18:06:47 +0530

    Add main.tf (generated by Azure OpenAI)

--- main.tf
+++ main.tf
@@ -1,1 +1,8 @@
-resource "azurerm_}
+resource "aws_s3_bucket" "my_bucket" {
+     bucket = "my-unique-bucket-name"  # Change this to a unique bucket name
+     acl    = "private"
+
+     tags = {
+       Name        = "My S3 Bucket"
+       Environment = "Dev"
+     }
