8b825238f44b21f45f26278231213ce42ca5136b
bhushankpmg <bhushantopre@kpmg.com>
2025-11-03 20:37:49 +0530

    Update main.tf after adjustments

--- main.tf
+++ main.tf
@@ -1,8 +1,4 @@
-resource "aws_s3_bucket" "my_bucket" {
-  bucket = "my-unique-bucket-name"  # Change to a unique bucket name
-  acl    = "private"
-
-  tags = {
-    Name        = "MyS3Bucket"
-    Environment = "production"
-  }
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "East US"
+}
