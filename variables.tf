a504cbe44751bf52165504ea8c1b0eb6a6caf382
bhushankpmg <bhushantopre@kpmg.com>
2025-11-03 18:16:58 +0530

    Add variables.tf (generated by Azure OpenAI)

--- variables.tf
+++ variables.tf
@@ -1,5 +1,4 @@
-variable "aws_region" {
-  description = "The AWS region to create the S3 bucket in."
+variable "bucket_name" {
+  description = "The name of the S3 bucket"
   type        = string
-  default     = "us-west-2"  # Adjust as needed
 }
