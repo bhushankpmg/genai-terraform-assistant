bc4ef34f02a5da4469abc2ef11b22fb52c63bdd8
bhushankpmg <bhushantopre@kpmg.com>
2025-11-11 16:51:33 +0530

    Update provider.tf after adjustments

--- provider.tf
+++ provider.tf
@@ -1,12 +1,2 @@
-provider in Terraform and authenticated with Azure for the current session.
-
-First, make sure you have the following prerequisites:
-- Terraform installed on your machine.
-- Azure CLI installed and configured to access your Azure account.
-- An active Azure subscription.
-
-Here's a simple Terraform script to create an Azure Storage Account:
-
-```hcl
 provider "azurerm" {
   features {}
