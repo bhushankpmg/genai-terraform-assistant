79b447fb4a8b13e98ef33cf0126cb8cd912025cf
bhushankpmg <bhushantopre@kpmg.com>
2025-12-01 22:43:14 +0530

    AI Create terraform/environments/dev/storage_account_tt-storage.tf

---
+++ terraform/environments/dev/storage_account_tt-storage.tf
@@ -0,0 +1,8 @@
+resource "azurerm_storage_account" "tt_storage" {
+  name                     = "ttstorage"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = "eastus"
+  account_tier             = "Standard"
+  account_replication_type  = "LRS"
+  tags                     = {}
+}
