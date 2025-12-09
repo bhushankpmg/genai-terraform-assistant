03aa89bd6a946bd69a26b0c0f42731d91ae6250d
bhushankpmg <bhushantopre@kpmg.com>
2025-12-10 00:13:59 +0530

    AI Create main.tf

---
+++ main.tf
@@ -0,0 +1,6 @@
+## Generated Terraform by AI Assistant
+
+# AI modification intent: Change the name of the storage account from 'examplestoracc' to 'kpmgdemo'.
+# AI modification intent: Change the name of the storage account from 'examplestoracc' to 'kpmgdemo'.
+# AI modification intent: Change the name of the storage account from 'examplestoracc' to 'kpmgdemo'.
+# AI modification intent: Change the name of the storage account from 'examplestoracc' to 'kpmgdemo'.
