6596f97e50c5a0424245831a7e7465e2fd84d59a
bhushankpmg <bhushantopre@kpmg.com>
2025-12-10 16:48:11 +0530

    AI Create: envs\dev\main.tf

---
+++ envs\dev\main.tf
@@ -0,0 +1,8 @@
+module "storage_dev" {
+    source                  = "../../modules/storage_account"
+    sa_name                 = "examplestoracc"
+    location                = "East US"
+    account_tier            = "Standard"
+    account_replication_type = "LRS"
+    resource_group_name      = "rg-dev-001"
+}
