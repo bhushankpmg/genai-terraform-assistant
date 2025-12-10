11097109b23986fc704dee380e829f8f17c202a0
bhushankpmg <bhushantopre@kpmg.com>
2025-12-10 15:48:48 +0530

    AI Create: envs\dev\main.tf

---
+++ envs\dev\main.tf
@@ -0,0 +1,8 @@
+module "storage_dev" {
+    source                  = "../../modules/storage_account"
+    sa_name                 = "kpmgdemo"
+    resource_group_name      = azurerm_resource_group.example.name
+    location                 = azurerm_resource_group.example.location
+    account_tier            = "Standard"
+    account_replication_type = "LRS"
+}
